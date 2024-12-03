9e4b804eb5364b1a8db78ad648d8e7e8ada9c99d
Hansajaa <nilanahansaja21@gmail.com>
2024-12-03 12:44:46 +0530

    Update redux store to work persistently with localstorage

--- src/app/Store.jsx
+++ src/app/Store.jsx
@@ -1,10 +1,32 @@
-import {configureStore} from '@reduxjs/toolkit'
-import cartReducer from '../feature/cart/CartSlice'
+import { configureStore } from '@reduxjs/toolkit'
 import UserReducer from '../feature/users/UserSlice'
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' //Default to localstorage
+import { combineReducers } from '@reduxjs/toolkit'
 
-export const Store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        users: UserReducer
-    }
+// Persist configuration
+const persistConfig = {
+    key: 'root',
+    storage, // Use localStorage
+};
+
+//combine reducers
+const rootReducer = combineReducers({
+    users: UserReducer,
 })
+
+//create a persist reducer
+const persistReducerObj = persistReducer(persistConfig,rootReducer);
+
+
+//Configure store
+const store = configureStore({
+    reducer: persistReducerObj
+})
+
+//persist store
+const persistor = persistStore(store);
+
+export {store, persistor};
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {Provider} from 'react-redux'
-import { Store } from './app/Store.jsx'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './app/Store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={Store}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
