ab5b868a82798e39dee556b8b7482d00d1946d01
Hansajaa <nilanahansaja21@gmail.com>
2024-12-04 16:56:54 +0530

    Hard coded product list

---
+++ src/Data/MenProducts.jsx
@@ -0,0 +1,94 @@
+import menImage from '../assets/Items/men_item_img.png'
+
+export const menPorductArray = [
+    {
+        id: 1,
+        description: "Classic Blue Hustler T-Shirt",
+        image_url: menImage,
+        name: "Royal Blue T-Shirt",
+        price: 1800,
+        productID: "p-000001",
+        quantity: 0
+    },
+    {
+        id: 2,
+        description: "Bold Red Hustler T-Shirt",
+        image_url: menImage,
+        name: "Crimson Red T-Shirt",
+        price: 2000,
+        productID: "p-000002",
+        quantity: 30
+    },
+    {
+        id: 3,
+        description: "Premium Black Hustler T-Shirt",
+        image_url: menImage,
+        name: "Jet Black T-Shirt",
+        price: 2200,
+        productID: "p-000003",
+        quantity: 20
+    },
+    {
+        id: 4,
+        description: "Vibrant Green Hustler T-Shirt",
+        image_url: menImage,
+        name: "Forest Green T-Shirt",
+        price: 1900,
+        productID: "p-000004",
+        quantity: 35
+    },
+    {
+        id: 5,
+        description: "Trendy Yellow Hustler T-Shirt",
+        image_url: menImage,
+        name: "Sunshine Yellow T-Shirt",
+        price: 1700,
+        productID: "p-000005",
+        quantity: 28
+    },
+    {
+        id: 6,
+        description: "Stylish Grey Hustler T-Shirt",
+        image_url: menImage,
+        name: "Ash Grey T-Shirt",
+        price: 2100,
+        productID: "p-000006",
+        quantity: 22
+    },
+    {
+        id: 7,
+        description: "Soft White Hustler T-Shirt",
+        image_url: menImage,
+        name: "Snow White T-Shirt",
+        price: 2300,
+        productID: "p-000007",
+        quantity: 18
+    },
+    {
+        id: 8,
+        description: "Cool Aqua Hustler T-Shirt",
+        image_url: menImage,
+        name: "Aqua Blue T-Shirt",
+        price: 1850,
+        productID: "p-000008",
+        quantity: 40
+    },
+    {
+        id: 9,
+        description: "Bold Orange Hustler T-Shirt",
+        image_url: menImage,
+        name: "Burnt Orange T-Shirt",
+        price: 1950,
+        productID: "p-000009",
+        quantity: 27
+    },
+    {
+        id: 10,
+        description: "Deep Purple Hustler T-Shirt",
+        image_url: menImage,
+        name: "Royal Purple T-Shirt",
+        price: 2500,
+        productID: "p-000010",
+        quantity: 15
+    }
+]
